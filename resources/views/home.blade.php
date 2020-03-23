d5c6e9184ae63fbc590404cb27952564f7dfd1e3
idstack <idstack@outlook.co.id>
2020-03-23 09:46:46 +0000

    create basic chat layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,43 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
+                <div class="card-body bg-secondary" style="height: 30rem; overflow: auto;">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="media m-2 bg-light rounded">
+                        <div class="media-body m-2">
+                          <h5 class="mt-0 ">Media heading</h5>
+                          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+
+                    <div class="media m-2 bg-light rounded">
+                        <div class="media-body m-2">
+                          <h5 class="mt-0 ">Media heading</h5>
+                          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+
+                    <div class="media m-2 bg-light rounded">
+                        <div class="media-body m-2">
+                          <h5 class="mt-0 ">Media heading</h5>
+                          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+                    <form action="#">
+                        <div class="form-group">
+                            <textarea name="" id="" cols="10" rows="3" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">Send</button>
+                    </form>
                 </div>
             </div>
         </div>
