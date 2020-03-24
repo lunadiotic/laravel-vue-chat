8901d7c9d4b8dd60d8fbd6137294715c4dc4cbc7
idstack <idstack@outlook.co.id>
2020-03-24 06:20:12 +0000

    test broadcast pusher

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ChatSent;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +25,10 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function sendChat(Request $request)
+    {
+        event(new ChatSent($request->user(), 'message'));
     }
 }
